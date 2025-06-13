cad966728ca8584b3bf0cf8f3098c106ef6596dc
Diego Pelaez <diego.pelaez@bettercloud.com>
2025-06-13 09:32:38 -0600

    Remove unused packages.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
